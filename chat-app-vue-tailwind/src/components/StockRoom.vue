4eb79c829de58c15f070cdcd24e52f2e5fcd8bd2
Ricards Taujenis <MozesThegreat@yahoo.com>
2021-10-19 16:48:40 +0200

    room template created

--- chat-app-vue-tailwind/src/components/StockRoom.vue
+++ chat-app-vue-tailwind/src/components/StockRoom.vue
@@ -13,7 +13,7 @@
                     <h2 class="ml-2 mb-2 text-gray-600 text-lg my-2">Chats</h2>
                     <li>
                         <a class="hover:bg-gray-100 border-b border-gray-300 px-3 py-2 cursor-pointer flex items-center text-sm focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                            
+                             
                             <div class="w-full pb-2">
                                 <div class="flex justify-between">
                                     <span class="block ml-2 font-semibold text-base text-gray-600 ">Jhon C</span>
