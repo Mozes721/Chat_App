a1bbfa993cf1bcc9e8bfc2895f246d3ace8b2fa8
Mozes721 <Richard.Taujenis@gmail.com>
2021-10-19 16:44:54 +0200

    room design created

--- chat-app-vue-tailwind/src/components/StockRoom.vue
+++ chat-app-vue-tailwind/src/components/StockRoom.vue
@@ -1,12 +1,99 @@
 <template>
   <!-- <h2>{{user}}</h2> -->
   <div class="bg-blue-400 h-screen w-screen overflow-hidden">
-    <h1 class="flex justify-center text-2xl">Stock Room test</h1>
-    <div class="flex flex-row rounded-lg shadow-lg items-center flex-2 h-full justify-center px-2 sm:px-0" >
-      <div class="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-3/4  sm:mx-0" style="height: 700px">
-        <div class="w-1/4  bg-green-500 flex justify-center">Room Members</div>
-        <div class="w-3/4  bg-blue-500 flex justify-center">Chat</div>
-      </div>
+    <div class="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0">
+    <div class="flex flex-cols-2 w-3/4 border rounded items-center justify-center mb-8" style="min-height: 80vh;">
+            <div class="w-1/4 h-full bg-white border-r border-gray-300">
+                <div class="my-3 mx-3 ">
+                    <button class=" bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                      Exit Room
+                    </button>
+                </div>
+                <ul class="overflow-auto" style="height: 500px;">
+                    <h2 class="ml-2 mb-2 text-gray-600 text-lg my-2">Chats</h2>
+                    <li>
+                        <a class="hover:bg-gray-100 border-b border-gray-300 px-3 py-2 cursor-pointer flex items-center text-sm focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                            
+                            <div class="w-full pb-2">
+                                <div class="flex justify-between">
+                                    <span class="block ml-2 font-semibold text-base text-gray-600 ">Jhon C</span>
+                                  
+                                </div>
+                              
+                            </div>
+                        </a>
+                        <a class="bg-gray-100 border-b border-gray-300 px-3 py-2 cursor-pointer flex items-center text-sm focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                          
+                            <div class="w-full pb-2">
+                                <div class="flex justify-between">
+                                    <span class="block ml-2 font-semibold text-base text-gray-600 ">Eduard</span>
+                                  
+                                </div>
+                             
+                            </div>
+                        </a>
+                        <a class="hover:bg-gray-100 border-b border-gray-300 px-3 py-2 cursor-pointer flex items-center text-sm focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                            
+                            <div class="w-full pb-2">
+                                <div class="flex justify-between">
+                                    <span class="block ml-2 font-semibold text-base text-gray-600 ">Celia</span>
+                               
+                                </div>
+                               
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-3/4 bg-white">
+                <div class="w-full">
+                    <div class="flex items-center border-b border-gray-300 pl-3 py-3">
+                      
+                        <span class="block ml-2 font-bold  text-gray-700 text-2xl align-middle">Stock Room</span><br>
+                        <h5 class="block ml-2 font-bold text-base text-gray-600 float-left">Eduard</h5>
+                      
+                    </div>
+                    <div id="chat" class="w-full overflow-y-auto p-10 relative" style="height: 700px;" ref="toolbarChat">
+                        <ul>
+                            <li class="clearfix2">
+                                <div class="w-full flex justify-start">
+                                    <div class="bg-gray-100 rounded px-5 py-2 my-2 text-gray-700 relative" style="max-width: 300px;">
+                                        <span class="block">Hello bro</span>
+                                        <span class="block text-xs text-right">10:30pm</span>
+                                    </div>
+                                </div>
+                                <div class="w-full flex justify-end" >
+                                    <div class="bg-gray-100 rounded px-5 py-2 my-2 text-gray-700 relative" style="max-width: 300px;">
+                                        <span class="block">Hello</span>
+                                        <span class="block text-xs text-left">10:32pm</span>
+                                    </div>
+                                </div>
+                                <div class="w-full flex justify-end" >
+                                    <div class="bg-gray-100 rounded px-5 py-2 my-2 text-gray-700 relative" style="max-width: 300px;">
+                                        <span class="block">how are you?</span>
+                                        <span class="block text-xs text-left">10:32pm</span>
+                                    </div>
+                                </div>
+            
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="w-full py-3 px-3 flex items-center justify-between border-t border-gray-300">
+                       
+
+                        <input aria-placeholder="Escribe un mensaje aquí" placeholder="Escribe un mensaje aquí"
+                            class="py-2 mx-3 pl-5 block w-full rounded-full bg-gray-100 outline-none focus:text-gray-700" type="text" name="message" required/>
+
+                        <button class="outline-none focus:outline-none" type="submit">
+                            <svg class="text-gray-400 h-7 w-7 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </template>
@@ -20,6 +107,11 @@ export default {
   },
    props: [
     'user'
-  ]
+  ],
+  methods: {
+    chooseRoom() {
+      alert()
+    }
+  }
 }
 </script>
