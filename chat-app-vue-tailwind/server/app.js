907c69d2d67026ef97de99264d2b4c55fcc246a7
Richard <MozesTheGreat@yahoo.com>
2021-11-01 19:34:20 +0200

    server adjusted to connect to client

--- chat-app-vue-tailwind/server/app.js
+++ chat-app-vue-tailwind/server/app.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const server = app.listen(3000, function() {
+    console.log('server running on port 3000');
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
+
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    console.log(socket.id)
+    socket.on('SEND_MESSAGE', function(data) {
+        io.emit('MESSAGE', data)
     });
-  });
-
-  
-server.listen(3000, () => {
-  console.log('listening on *:3000');
 });
-
--- chat-app-vue-tailwind/src/components/StockRoom.vue
+++ chat-app-vue-tailwind/src/components/StockRoom.vue
@@ -81,15 +81,16 @@
 
                     <div class="w-full py-3 px-3 flex items-center justify-between border-t border-gray-300">
                        
+                        <form @submit.prevent="sendMessage">
+                            <input aria-placeholder="Enter your message" placeholder="Enter your message" type="text" v-model="message"
+                                class="py-2 mx-3 pl-5 block w-full rounded-full bg-gray-100 outline-none focus:text-gray-700" name="message" required/>
 
-                        <input aria-placeholder="Enter your message" placeholder="Enter your message"
-                            class="py-2 mx-3 pl-5 block w-full rounded-full bg-gray-100 outline-none focus:text-gray-700" type="text" name="message" required/>
-
-                        <button class="outline-none focus:outline-none" type="submit">
-                            <svg class="text-gray-400 h-7 w-7 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-                            </svg>
-                        </button>
+                            <button class="outline-none focus:outline-none" type="submit">
+                                <svg class="text-gray-400 h-7 w-7 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                                </svg>
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -100,6 +101,8 @@
  
 <script>
 import EnterRoom from './EnterRoom.vue'
+import io from 'socket.io-client';
+
 export default {
   name:'StockRoom',
   components:{
@@ -108,15 +111,30 @@ export default {
    props: [
     'user'
   ],
-//   data() {
-//       return {
-//       user: '',
-//       message: '',
-//       messages: [],
-//       socket: io('localhost:3000')
-//       }
-//   },
+  data() {
+      return {
+    //   user: '',
+      message: '',
+      messages: [],
+      socket: io('localhost:3000')
+      }
+  },
   methods: {
+    sendMessage(e) {
+            e.preventDefault();
+            
+            this.socket.emit('SEND_MESSAGE', {
+                // user: this.user,
+                message: this.message
+            });
+            this.message = ''
+        },
+    //     mounted() {
+    //     this.socket.on('MESSAGE', (data) => {
+    //         this.messages = [...this.messages, data];
+    //         // you can also do this.messages.push(data)
+    //     });
+    // },
     chooseRoom() {
       alert()
     }
