69a9aaaa94d908e0d09da3a51a1d5134f4197f72
Mozes721 <Richard.Taujenis@gmail.com>
2021-11-02 19:23:12 +0100

    changed socketio

--- chat-app-vue-tailwind/server/app.js
+++ chat-app-vue-tailwind/server/app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
 
-const server = app.listen(3000, function() {
-    console.log('server running on port 3000');
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
 });
 
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
 
 const io = require('socket.io')(server);
 
-io.on('connection', function(socket) {
-    console.log(socket.id)
-    socket.on('SEND_MESSAGE', function(data) {
-        io.emit('MESSAGE', data)
-    });
-});
+io.on("connection", (socket) => {
+  console.log("connected to " + socket.id) //this is successfully getting logged
+  socket.on("SEND_MESSAGE", (msg) => {
+    console.log(socket.id + ": " + msg) //this is successfully getting logged when I click send
+    io.sockets.emit("chat", msg) 
+  } )
+} )
--- chat-app-vue-tailwind/src/components/StockRoom.vue
+++ chat-app-vue-tailwind/src/components/StockRoom.vue
@@ -78,20 +78,19 @@
                             </li>
                         </ul>
                     </div>
+                     <form @submit.prevent="sendMessage" action="">
+                        <div class="w-full py-3 px-3 flex items-center justify-between border-t border-gray-300">
+                        
+                                <input aria-placeholder="Enter your message" placeholder="Enter your message" type="text" v-model="message"
+                                    class="py-2 mx-3 pl-5 block w-full rounded-full bg-gray-100 outline-none focus:text-gray-700" name="message" required/>
 
-                    <div class="w-full py-3 px-3 flex items-center justify-between border-t border-gray-300">
-                       
-                        <form @submit.prevent="sendMessage">
-                            <input aria-placeholder="Enter your message" placeholder="Enter your message" type="text" v-model="message"
-                                class="py-2 mx-3 pl-5 block w-full rounded-full bg-gray-100 outline-none focus:text-gray-700" name="message" required/>
-
-                            <button class="outline-none focus:outline-none" type="submit">
-                                <svg class="text-gray-400 h-7 w-7 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-                                </svg>
-                            </button>
-                        </form>
-                    </div>
+                                <button class="outline-none focus:outline-none" type="submit">
+                                    <svg class="text-gray-400 h-7 w-7 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                                    </svg>
+                                </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
