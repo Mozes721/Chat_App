13061c8e3bcaba9e81305b675a05e4b0defa013e
Ricards Taujenis <MozesThegreat@yahoo.com>
2021-12-18 00:20:59 +0100

    added the username to apear when message send

--- chat-app-vue-tailwind/src/components/ChatRoom.vue
+++ chat-app-vue-tailwind/src/components/ChatRoom.vue
@@ -19,7 +19,7 @@
                                     <span class="block ml-2 font-semibold text-base text-gray-600 ">{{this.user}}</span>
                                   
                                 </div>
-                              
+                               
                             </div>
                         </a>
                         <!-- <a class="bg-gray-100 border-b border-gray-300 px-3 py-2 cursor-pointer flex items-center text-sm focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
